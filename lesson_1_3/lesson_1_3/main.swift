8152bd128fa583d336d6bd00165ba488ea1a0785
timokhina <annamaksimovna@gmail.com>
2019-06-18 02:02:09 +0300

    Добавлены задания 1.1 и 1.2

---
+++ lesson_1_3/lesson_1_3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lesson_1_3
+//
+//  Created by Anna Timokhina on 18/06/2019.
+//  Copyright © 2019 Anna Timokhina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
